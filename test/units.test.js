0140ec8b13c2e0fa093635b79dd06906155956bd
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-08-19 11:14:27 +0300

    Add update complete and clear test

--- test/units.test.js
+++ test/units.test.js
@@ -25,4 +25,41 @@ describe('Add and Remove a task', () => {
     todoList.removeTask(1);
     expect(todoList.list.length).toBe(0);
   });
+
+  test('Editing an Item', () => {
+    const todo = {
+      id: 1,
+      description: 'Learn React',
+      completed: false,
+    };
+
+    todoList.addTask(todo);
+    todoList.editTask(1, 'Learn React Native');
+    expect(todoList.list[0].description).toBe('Learn React Native');
+  });
+
+  test('Completing an Item', () => {
+    const todo = {
+      id: 1,
+      description: 'Learn React',
+      completed: false,
+    };
+
+    todoList.addTask(todo);
+    todoList.completeTask(1, true);
+    expect(todoList.list[0].completed).toBe(true);
+  });
+
+  test('Clearing completed items', () => {
+    const todo = {
+      id: 1,
+      description: 'Learn React',
+      completed: false,
+    };
+
+    todoList.addTask(todo);
+    todoList.completeTask(1, true);
+    todoList.clearCompletedTask();
+    expect(todoList.list.length).toBe(2);
+  });
 });
